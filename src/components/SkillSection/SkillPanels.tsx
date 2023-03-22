14b85e67e144c75e6a214565ac566d15d77e1d64
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-03-22 19:06:51 +0900

    🚧 SkillSectionをソートできるように

--- src/components/SkillSection/SkillPanels.tsx
+++ src/components/SkillSection/SkillPanels.tsx
@@ -1,20 +1,42 @@
-import { SimpleGrid } from '@mantine/core';
+import { css } from '@emotion/react';
+import { SegmentedControl, SimpleGrid } from '@mantine/core';
 import type { FC } from 'react';
 import { SkillPanel } from '@/components/SkillSection/SkillPanel';
+import {
+  sortTabData,
+  useSKillSorting,
+} from '@/components/SkillSection/uesSkillSorting';
 import { skills } from '@/lib/skills';
 
 export const SkillPanels: FC = () => {
+  const { sortTab, onChangeSortTab, sortedSkills } = useSKillSorting(
+    'category',
+    skills,
+  );
+
   return (
-    <SimpleGrid
-      cols={3}
-      breakpoints={[
-        { maxWidth: 1160, cols: 2 },
-        { maxWidth: 800, cols: 1 },
-      ]}
-    >
-      {skills.map((skill) => (
-        <SkillPanel key={skill.name} skill={skill} />
-      ))}
-    </SimpleGrid>
+    <div>
+      <SegmentedControl
+        color="teal"
+        value={sortTab}
+        onChange={onChangeSortTab}
+        data={sortTabData}
+        css={css`
+          margin-bottom: 16px;
+        `}
+      />
+
+      <SimpleGrid
+        cols={3}
+        breakpoints={[
+          { maxWidth: 1160, cols: 2 },
+          { maxWidth: 800, cols: 1 },
+        ]}
+      >
+        {sortedSkills.map((skill) => (
+          <SkillPanel key={skill.name} skill={skill} />
+        ))}
+      </SimpleGrid>
+    </div>
   );
 };
---
+++ src/components/SkillSection/uesSkillSorting.ts
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import type { skill } from '@/lib/skills';
+
+type sortTabId = 'category' | 'level';
+
+export const sortTabData: { value: sortTabId; label: string }[] = [
+  { value: 'category', label: 'Sort by Category' },
+  { value: 'level', label: 'Sort by Level' },
+];
+
+type sortFunction = (a: skill, b: skill) => number;
+
+const getSortFunction = (sortTab: sortTabId): sortFunction => {
+  switch (sortTab) {
+    case 'category':
+      return () => 1;
+    case 'level':
+      return (a, b) => b.level - a.level;
+    default: {
+      const _exhaustiveCheck: never = sortTab;
+      throw new Error('exhaustiveCheck failed', sortTab);
+    }
+  }
+};
+
+type skillSortingObject = {
+  sortTab: string;
+  onChangeSortTab: (newTab: sortTabId) => void;
+  sortedSkills: skill[];
+};
+
+export const useSKillSorting = (
+  defaultTab: sortTabId,
+  skills: skill[],
+): skillSortingObject => {
+  const [sortTab, setSortTab] = useState<sortTabId>(defaultTab);
+
+  const sortedSkills = skills.slice().sort(getSortFunction(sortTab));
+
+  const onChangeSortTab = (newTab: sortTabId) => {
+    setSortTab(newTab);
+  };
+
+  return { sortTab, onChangeSortTab, sortedSkills };
+};
