920687ce214417c922c7dcfd7020e42c81707949
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-03-23 08:14:07 +0900

    ♻️ WorkPanelとArticlePanelをClickablePanelとして共通化

--- src/components/ArticleSection/ArticlePanel.tsx
+++ src/components/ArticleSection/ArticlePanel.tsx
@@ -1,11 +1,10 @@
 import { css } from '@emotion/react';
 import { AspectRatio, Card, useMantineTheme } from '@mantine/core';
 import dayjs from 'dayjs';
-import Link from 'next/link';
 import type { FC } from 'react';
+import { ClickablePanel } from '@/components/ClickablePanel/ClickablePanel';
 import { useColorScheme } from '@/components/MantineProvider/useColorScheme';
 import type { article } from '@/lib/articles';
-import { cardHoverTransition } from '@/styles/cardHoverTransition';
 
 type ArticlePanelProps = {
   article: article;
@@ -16,53 +15,33 @@ export const ArticlePanel: FC<ArticlePanelProps> = ({ article }) => {
   const { colors } = useMantineTheme();
 
   return (
-    <Link
-      href={article.url}
-      target="_blank"
-      rel="noopener noreferrer"
-      css={css`
-        text-decoration: none;
-      `}
-    >
-      <Card
-        shadow="sm"
-        padding="lg"
-        radius="md"
+    <ClickablePanel url={article.url}>
+      <Card.Section>
+        <AspectRatio ratio={1200 / 630}>
+          <img
+            src={`/api/fetch-og-image?url=${encodeURIComponent(article.url)}`}
+            alt=""
+          />
+        </AspectRatio>
+      </Card.Section>
+      <p
         css={css`
-          display: flex;
-          height: 100%;
-          flex-direction: column;
-
-          ${cardHoverTransition}
+          margin-top: 16px;
+          color: ${color(colors.gray[8], colors.gray[3])};
+          font-weight: bold;
         `}
       >
-        <Card.Section>
-          <AspectRatio ratio={1200 / 630}>
-            <img
-              src={`/api/fetch-og-image?url=${encodeURIComponent(article.url)}`}
-              alt=""
-            />
-          </AspectRatio>
-        </Card.Section>
-        <p
-          css={css`
-            margin-top: 16px;
-            color: ${color(colors.gray[8], colors.gray[3])};
-            font-weight: bold;
-          `}
-        >
-          {article.title}
-        </p>
-        <p
-          css={css`
-            padding-top: 8px;
-            margin-top: auto;
-            color: ${color(colors.gray[6], colors.gray[5])};
-          `}
-        >
-          {dayjs(article.created_at).format('YYYY/MM/DD')}
-        </p>
-      </Card>
-    </Link>
+        {article.title}
+      </p>
+      <p
+        css={css`
+          padding-top: 8px;
+          margin-top: auto;
+          color: ${color(colors.gray[6], colors.gray[5])};
+        `}
+      >
+        {dayjs(article.created_at).format('YYYY/MM/DD')}
+      </p>
+    </ClickablePanel>
   );
 };
--- src/components/WorkSection/WorkPanel.tsx
+++ src/components/WorkSection/WorkPanel.tsx
@@ -1,11 +1,10 @@
 import { css } from '@emotion/react';
 import { AspectRatio, Card, useMantineTheme } from '@mantine/core';
 import Image from 'next/image';
-import Link from 'next/link';
 import type { FC } from 'react';
+import { ClickablePanel } from '@/components/ClickablePanel/ClickablePanel';
 import { useColorScheme } from '@/components/MantineProvider/useColorScheme';
 import type { work } from '@/lib/works';
-import { cardHoverTransition } from '@/styles/cardHoverTransition';
 
 type WorkPanelProps = {
   work: work;
@@ -16,57 +15,41 @@ export const WorkPanel: FC<WorkPanelProps> = ({ work }) => {
   const { colors } = useMantineTheme();
 
   return (
-    <Link
-      href={`works/${work}`}
-      css={css`
-        text-decoration: none;
-      `}
-    >
-      <Card
-        shadow="sm"
-        padding="lg"
-        radius="md"
+    <ClickablePanel url={`works/${work}`}>
+      <Card.Section withBorder>
+        <AspectRatio
+          ratio={960 / 540}
+          css={css`
+            position: relative;
+            width: 100%;
+          `}
+        >
+          <Image
+            src={`/og-images/${work.id}.png`}
+            alt=""
+            fill
+            css={css`
+              object-fit: contain;
+            `}
+          />
+        </AspectRatio>
+      </Card.Section>
+      <p
         css={css`
-          height: 100%;
-
-          ${cardHoverTransition}
+          margin-top: 16px;
+          color: ${color(colors.gray[8], colors.gray[3])};
+          font-weight: bold;
         `}
       >
-        <Card.Section withBorder>
-          <AspectRatio
-            ratio={960 / 540}
-            css={css`
-              position: relative;
-              width: 100%;
-            `}
-          >
-            <Image
-              src={`/og-images/${work.id}.png`}
-              alt=""
-              fill
-              css={css`
-                object-fit: contain;
-              `}
-            />
-          </AspectRatio>
-        </Card.Section>
-        <p
-          css={css`
-            margin-top: 16px;
-            color: ${color(colors.gray[8], colors.gray[3])};
-            font-weight: bold;
-          `}
-        >
-          {work.name}
-        </p>
-        <p
-          css={css`
-            color: ${color(colors.gray[7], colors.gray[4])};
-          `}
-        >
-          {work.description}
-        </p>
-      </Card>
-    </Link>
+        {work.name}
+      </p>
+      <p
+        css={css`
+          color: ${color(colors.gray[7], colors.gray[4])};
+        `}
+      >
+        {work.description}
+      </p>
+    </ClickablePanel>
   );
 };
