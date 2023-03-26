970bacf5a649efb0f7defd5b5206cb57636b83f2
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-03-26 15:54:54 +0900

    🎨 TitleHeadingの文字色微修正

--- src/components/Layout/TitleHeading.tsx
+++ src/components/Layout/TitleHeading.tsx
@@ -1,11 +1,16 @@
 import { css } from '@emotion/react';
 import type { TitleProps } from '@mantine/core';
+import { useMantineTheme } from '@mantine/core';
 import { Title } from '@mantine/core';
 import type { FC, HTMLAttributes } from 'react';
+import { useColorScheme } from '@/components/MantineProvider/useColorScheme';
 
 type TitleHeadingProps = TitleProps & HTMLAttributes<HTMLHeadingElement>;
 
 export const TitleHeading: FC<TitleHeadingProps> = (props) => {
+  const { color } = useColorScheme();
+  const { colors } = useMantineTheme();
+
   return (
     <Title
       order={1}
@@ -14,6 +19,7 @@ export const TitleHeading: FC<TitleHeadingProps> = (props) => {
       css={css`
         margin-bottom: 64px;
       `}
+      color={color(colors.gray[9], colors.gray[2])}
       {...props}
     />
   );
